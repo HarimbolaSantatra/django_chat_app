d61eecdbc8bfec13cee7fae804594b757a425fa7
santatra <razafindratsimba34@gmail.com>
2023-05-05 20:09:40 +0000

    ui: stable layout

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required   # For login required
 from django.contrib.auth.models import User
 from main_app.models import Room, Chat
 
+import json
 
 @login_required(login_url='login')
 def index(request):
@@ -19,12 +20,19 @@ def room(request, room_name):
     username = request.session['username']
     current_room = Room.objects.get(name=room_name)
     chats = Chat.objects.filter(room=current_room)
-    chat_message = ""
+    chat_messages = []
     for chat in chats:
-        chat_message += chat.user.username + " : " + chat.message + "\n"
+        isOwner = (chat.user.username == username)
+        class_name = "primary-message-box" if isOwner else "secondary-message-box"
+        new_chat = { 
+            "username":chat.user.username, 
+            "message":chat.message,
+            "class_name": class_name
+            }
+        chat_messages.append(new_chat)
     context = {
         'username': username,
         'room_name': room_name,
-        'chat_message': chat_message
+        'chat_message': chat_messages
     }
     return render(request, "room.html", context)
